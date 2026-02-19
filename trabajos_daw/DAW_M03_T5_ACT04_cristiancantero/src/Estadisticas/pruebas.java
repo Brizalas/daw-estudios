0b1c998ff2199e692fd1b7b5ce82d7dacc65da21
Cristian <cristian.cantero77@gmail.com>
2026-02-19 23:15:46 +0100

    ejercicio gptero con comidas d ollas mias... mañana lo termino de arreglar

--- trabajos_daw/DAW_M03_T5_ACT04_cristiancantero/src/Estadisticas/pruebas.java
+++ trabajos_daw/DAW_M03_T5_ACT04_cristiancantero/src/Estadisticas/pruebas.java
@@ -12,13 +12,13 @@ public class pruebas {
     public static void main (String[]args){
         double [] array = {5, 7.3, 4, 9.4};
         double suma = 0;
-        
+        //práctica for y for/each
         for(int i = 0 ; i < array.length; i++){
            suma = suma+array[i];
         }
         System.out.println(suma);
         
-        suma = 0;
+        suma = 0; //reinicia si no te suma la cantidad anterior XD
         for(double notas: array){
             suma += notas;
         }
