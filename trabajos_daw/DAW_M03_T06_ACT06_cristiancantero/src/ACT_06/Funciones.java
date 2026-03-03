f7346d9c027c0bcead39893ea8876024830878e5
Cristian Cantero <cristiancantero@MacBook-Pro-de-Cristian.local>
2026-03-03 09:38:36 +0100

    ejericicio lectura escritura secuencial. Primera funcion terminada

---
+++ trabajos_daw/DAW_M03_T06_ACT06_cristiancantero/src/ACT_06/Funciones.java
@@ -0,0 +1,56 @@
+package ACT_06;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author cristiancantero
+ */
+public class Funciones {
+    /**
+     * Función crear archivo
+     * Objetos File, creación de ruta, separador y archivo
+     * El usuario escoje el nombre del .txt que va a crear
+     * @param scanner sc
+     * 
+     */
+    
+    public static void nuevoArchivo(Scanner sc)throws IOException{
+         String ruta = System.getProperty("user.dir");
+         String sep = File.separator;
+         File archivo = new File(ruta+sep+"archivos");
+         if(!archivo.exists()){
+             System.out.println("Este archivo no existe");
+             System.out.println("Creando archivo...");
+             archivo.mkdir();
+             System.out.println("Archivo creado en: " + archivo.getAbsolutePath());
+         }else{
+             System.out.println("Esta carpeta existe en: " + archivo.getAbsolutePath());
+         }
+         System.out.println("Introduce el nombre del archivo.txt que quieres crear: ");
+         String nombre = sc.nextLine();
+         
+         File texto = new File(archivo,nombre);
+         if(!texto.exists()){
+             texto.createNewFile();
+             System.out.println("Creando hoja de registro en: " + texto.getAbsolutePath());
+         }
+         //escritura del .txt
+         System.out.println("Escribe lo que quieras en tu archivo " + nombre);
+         String escribir = sc.nextLine();
+         try(BufferedWriter bw = new BufferedWriter(new FileWriter(texto, true))){
+             bw.write(escribir);
+             bw.newLine();
+         }catch(IOException e){
+             System.out.println("Error en la escritura");
+         }
+         System.out.println("Esto es lo que has escrito en tu archivo " + nombre);
+         System.out.println(escribir);
+         
+    }
+   
+}
---
+++ trabajos_daw/DAW_M03_T06_ACT06_cristiancantero/src/ACT_06/Main.java
@@ -0,0 +1,16 @@
+package ACT_06;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author cristiancantero
+ */
+public class Main {
+    public static void main(String[]args)throws IOException{
+        Scanner sc = new Scanner (System.in);
+        Funciones.nuevoArchivo(sc);
+    }
+}
