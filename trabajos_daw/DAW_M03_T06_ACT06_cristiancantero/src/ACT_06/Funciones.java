151c8a5ce43bc12ea28a94472a9cbf54f4a71ab0
Cristian Cantero <cristiancantero@MacBook-Pro-de-Cristian.local>
2026-03-09 08:44:47 +0100

    funcion listar archivos terminada

--- trabajos_daw/DAW_M03_T06_ACT06_cristiancantero/src/ACT_06/Funciones.java
+++ trabajos_daw/DAW_M03_T06_ACT06_cristiancantero/src/ACT_06/Funciones.java
@@ -11,53 +11,86 @@ import java.util.Scanner;
  * @author cristiancantero
  */
 public class Funciones {
+
     /**
-     * Función crear archivo
-     * Objetos File, creación de ruta, separador y archivo
+     * Función crear archivo Objetos File, creación de ruta, separador y archivo
      * El usuario escoje el nombre del .txt que va a crear
+     *
      * @param scanner sc
-     * 
+     *
      */
-    
-    public static void nuevoArchivo(Scanner sc)throws IOException{
-         String ruta = System.getProperty("user.dir");
-         String sep = File.separator;
-         File archivo = new File(ruta+sep+"archivos");
-         if(!archivo.exists()){
-             System.out.println("Este archivo no existe");
-             System.out.println("Creando archivo...");
-             archivo.mkdir();
-             System.out.println("Archivo creado en: " + archivo.getAbsolutePath());
-         }else{
-             System.out.println("Esta carpeta existe en: " + archivo.getAbsolutePath());
-         }
-         System.out.println("Introduce el nombre del archivo.txt que quieres crear: ");
-         String nombre = sc.nextLine();
-         
-         File texto = new File(archivo,nombre);
-         if(!texto.exists()){
-             texto.createNewFile();
-             System.out.println("Creando hoja de registro en: " + texto.getAbsolutePath());
-         }
-         //escritura del .txt
-         System.out.println("Escribe lo que quieras en tu archivo " + texto);
-         String escribir = sc.nextLine();
-         try(BufferedWriter bw = new BufferedWriter(new FileWriter(texto, true))){
-             bw.write(escribir);
-             bw.newLine();
-         }catch(IOException e){
-             System.out.println("Error en la escritura");
-         }
-         System.out.println("Esto es lo que has escrito en tu archivo " + nombre);
-         System.out.println(escribir);
-         
-    }
-    
-    public static File [] listarArchivos(){
+
+    public static void nuevoArchivo(Scanner sc) throws IOException {
         String ruta = System.getProperty("user.dir");
         String sep = File.separator;
-        File archivo = new File(ruta+sep+"archivos");
-        
+        File archivo = new File(ruta + sep + "archivos");
+        if (!archivo.exists()) {
+            System.out.println("Este archivo no existe");
+            System.out.println("Creando archivo...");
+            archivo.mkdir();
+            System.out.println("Archivo creado en: " + archivo.getAbsolutePath());
+        } else {
+            System.out.println("Esta carpeta existe en: " + archivo.getAbsolutePath());
+        }
+        System.out.println("Introduce el nombre del archivo.txt que quieres crear: ");
+        String nombre = sc.nextLine();
+
+        File texto = new File(archivo, nombre);
+        if (!texto.exists()) {
+            texto.createNewFile();
+            System.out.println("Creando hoja de registro en: " + texto.getAbsolutePath());
+        }
+        //escritura del .txt
+        System.out.println("Escribe lo que quieras en tu archivo " + texto);
+        String escribir = sc.nextLine();
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(texto, true))) {
+            bw.write(escribir);
+            bw.newLine();
+        } catch (IOException e) {
+            System.out.println("Error en la escritura");
+        }
+        System.out.println("Esto es lo que has escrito en tu archivo " + nombre);
+        System.out.println(escribir);
+
     }
-   
+
+    public static File[] listarArchivos() {
+        String ruta = System.getProperty("user.dir");
+        String sep = File.separator;
+        File archivo = new File(ruta + sep + "archivos");
+        if (!archivo.exists()) {
+            System.out.println("Esta carpeta no existe");
+            archivo.mkdir();
+        }
+        File[] contenido = archivo.listFiles();
+        
+        if(contenido == null){
+            return new File[0];
+            
+        }
+        int contador = 0; //este primer contador es para saber cuantos .txt válidos hay
+        for (File f : contenido){
+            if(f.isFile() && f.getName().endsWith(".txt")){
+                contador ++;
+            }
+        }
+        File[] resultado = new File[contador];
+        int indice = 0; // este índice sirve para saber en qué posición (hueco) del array []resultado estoy guardando.
+        for (File f : contenido){
+            if(f.isFile() && f.getName().endsWith(".txt")){
+            resultado [indice]= f;
+            indice ++;
+            }
+        }
+        
+        for(int i = 0; i < resultado.length; i ++){
+            System.out.println((i+1)+" - "+ resultado[i].getName());
+        }
+        return resultado;
+        
+
+    }
+    
+
+
 }
--- trabajos_daw/DAW_M03_T06_ACT06_cristiancantero/src/ACT_06/Main.java
+++ trabajos_daw/DAW_M03_T06_ACT06_cristiancantero/src/ACT_06/Main.java
@@ -1,5 +1,6 @@
 package ACT_06;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -11,6 +12,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[]args)throws IOException{
         Scanner sc = new Scanner (System.in);
-        Funciones.nuevoArchivo(sc);
+        File [] resultado = Funciones.listarArchivos();
+        
+        
+        
+        
     }
 }
