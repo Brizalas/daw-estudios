80c23d5cd011c0a09b799becfa666f2afe8eee94
Cristian Cantero <tuemail@github.com>
2026-04-12 14:47:23 +0200

    Funciones actualizar producto y eliminar producto terminadas

--- trabajos_daw/DAW_M03_T10_ACT10_CristianCantero/src/DAW_M03_ACT10_T10/Main.java
+++ trabajos_daw/DAW_M03_T10_ACT10_CristianCantero/src/DAW_M03_ACT10_T10/Main.java
@@ -13,44 +13,45 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Funciones f = new Funciones();
+        Producto p = new Producto();
         HashMap<String, Producto> listaProductos = new HashMap<>();
         ArrayList<String> materiales = new ArrayList<>();
         boolean valido = false;
         
-        Producto p1 = new Producto("Mesa",materiales,100,5);
-        listaProductos.put("P001",p1);
-        
-        f.ingresarProducto(listaProductos, p1);
+       
+        do {
+            try {
+                System.out.println("====Menú====");
+                System.out.println("1. Agregar producto");
+                System.out.println("2. Actualizar producto");
+                System.out.println("3. Eliminar producto");
+                System.out.println("0. Salir");
+                System.out.println("=============");
+                int opcion = sc.nextInt();
+                sc.nextLine();
 
-//        do {
-//            try {
-//                System.out.println("====Menú====");
-//                System.out.println("1. Agregar producto");
-//                System.out.println("2. Actualizar producto");
-//                System.out.println("3. Eliminar producto");
-//                System.out.println("0. Salir");
-//                System.out.println("=============");
-//                int opcion = sc.nextInt();
-//                sc.nextLine();
-//
-//                switch (opcion) {
-//                    case 1:
-//                        break;
-//                    case 2:
-//                        break;
-//                    case 3:
-//                        break;
-//                    case 0:
-//                        System.out.println("Adios");
-//                        valido = true;
-//                        break;
-//                    default:
-//                        System.out.println("Introduce una opción válida");
-//                }
-//            } catch (Exception e) {
-//                System.out.println("Error al validar");
-//            }
-//        } while (!valido);
+                switch (opcion) {
+                    case 1:
+                        f.ingresarProducto(listaProductos, p);
+                        break;
+                    case 2:
+                        f.actualizarProducto(sc, listaProductos);
+                        break;
+                        
+                    case 3:
+                        f.eliminarProducto(sc, listaProductos); 
+                        break;
+                    case 0:
+                        System.out.println("Adios");
+                        valido = true;
+                        break;
+                    default:
+                        System.out.println("Introduce una opción válida");
+                }
+            } catch (Exception e) {
+                System.out.println("Error al validar");
+            }
+        } while (!valido);
 
     }
 }
